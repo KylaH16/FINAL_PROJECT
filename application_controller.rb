afc7a99f3d1329fcae6f94b0f919c32c2c0797b9
Bea Jones <beatrixdjones@gmail.com>
2015-06-19 20:24:16 +0000

    embeds ruby

--- application_controller.rb
+++ application_controller.rb
@@ -1,19 +1,18 @@
-#video game controller
-
 require 'bundler'
 Bundler.require
 require_relative 'models/model.rb'
+require_relative 'models/imagegetter.rb'
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
   end
   post '/'do
-    Responder =Result.new
-    name = params[:name]
-    emo =params[:emotion]
-    puts params
-    @answer= Responder.response(name,emo)
+    Responder = Emoji.new(params)
+   # @params = params
+    @answer= Responder.result
+    Image = ImageGetter.new
+    @url_array = Image.convert(@answer)
     #respond.erb
     erb :respond
   end
--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,14 @@
 class Emoji
-  def initialize
+  def initialize(params)
+    #TEST PARAMS ARRAY
+#     params={
+#       :tone => "happy",
+#       :time => "day",
+#       :feel => "indiff",
+#       :pers => "chill",
+#       :react => "okay"
+#       }
+    @params = params
     @emoji={
       :smile => 0,
       :confused => 0,
@@ -10,81 +19,89 @@ class Emoji
  
   #question one results
   def q1
-    if params[:tone] == "happy"
+    if @params[:tone] == "happy"
       @emoji[:smile] += 1
-    elsif params[:tone] == "angry"
+    elsif @params[:tone] == "angry"
       @emoji[:grimace] += 1
-    elsif params[:tone] == "sad"
+    elsif @params[:tone] == "sad"
       @emoji[:confused] += 1
-    elsif params[:tone] == "bored"
+    elsif @params[:tone] == "bored"
       @emoji[:thumbs_up] += 1
     end
   end
   
   #question two results
   def q2
-    if params[:time] == "day"
+    if @params[:time] == "day"
       @emoji[:smile] += 1
-    elsif params[:time] == "month"
+    elsif @params[:time] == "month"
       @emoji[:thumbs_up] += 1
-    elsif params[:time] == "year"
+    elsif @params[:time] == "year"
       @emoji[:confused] += 1
-    elsif params[:time] == "forever"
+    elsif @params[:time] == "forever"
       @emoji[:grimace] += 1
     end
   end
   
   #question three results
   def q3
-    if params[:feel] == "annoyed"
+    if @params[:feel] == "annoyed"
       @emoji[:grimace] += 1
-    elsif params[:feel] == "indiff"
+    elsif @params[:feel] == "indiff"
       @emoji[:thumbs_up] += 1
-    elsif params[:feel] == "confused"
+    elsif @params[:feel] == "confused"
       @emoji[:confused] += 1
-    elsif params[:feel] == "cheerful"
+    elsif @params[:feel] == "cheerful"
       @emoji[:smile] += 1
     end
   end
   
     #question four results
   def q4
-    if params[:pers] == "serious"
+    if @params[:pers] == "serious"
       @emoji[:grimace] += 1
-    elsif params[:pers] == "chill"
+    elsif @params[:pers] == "chill"
       @emoji[:thumbs_up] += 1
-    elsif params[:pers] == "funny"
+    elsif @params[:pers] == "funny"
       @emoji[:smile] += 1
-    elsif params[:pers] == "indecisive"
+    elsif @params[:pers] == "indecisive"
       @emoji[:confused] += 1
     end
   end
   
   #question five results
   def q5
-    if params[:react] == "hahaha"
+    if @params[:react] == "hahaha"
       @emoji[:smile] += 1
-    elsif params[:react] == "what"
+    elsif @params[:react] == "what"
       @emoji[:confused] += 1
-    elsif params[:react] == "ugh"
+    elsif @params[:react] == "ugh"
       @emoji[:grimace] += 1
-    elsif params[:react] == "okay"
+    elsif @params[:react] == "okay"
       @emoji[:thumbs_up] += 1
     end
   end
   
   def result
+    #run questions
     q1
     q2
     q3
     q4
     q5
+    #results
     values = @emoji.values
-    puts values
-    
+    max = values.max
+    answers =[]
+    #looking through the hash for matches
+    @emoji.each do |emoji,score|
+      if score == max
+        answers.push(emoji)
+      end
+    end
+    return answers
   end
 end
 
-
-Test = Emoji.new
-puts Test.result
+# Test = Emoji.new
+# puts Test.result
